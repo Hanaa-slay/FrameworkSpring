80c23335cb700a43c00843ffcdfd6f11ee87851d
Hanaa-slay <hanaatdr@gmail.com>
2026-07-14 09:46:24 +0300

    sprint 4

--- src/main/java/com/mybf/listener/AppStartListener.java
+++ src/main/java/com/mybf/listener/AppStartListener.java
@@ -1,6 +1,9 @@
 package com.mybf.listener;
 
 import com.mybf.annotation.Controller;
+import com.mybf.annotation.UrlMapping;
+import com.mybf.utils.RouteMapping;
+import com.mybf.utils.UrlMethod;
 import com.mybf.utils.Utilitaire;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextEvent;
@@ -8,6 +11,8 @@ import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
 
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 @WebListener
 public class AppStartListener implements ServletContextListener {
@@ -15,10 +20,22 @@ public class AppStartListener implements ServletContextListener {
         ServletContext context = sce.getServletContext();
         Utilitaire util = new Utilitaire();
         String packageName = context.getInitParameter("packageController");
+        String prefix = context.getInitParameter("prefix");
+        String suffix = context.getInitParameter("suffix");
+
         try {
             //List<String> controllers = util.getListController(packageName, Controller.class);
-            List<Class<?>> controllers = util.getListControllerClass(packageName, Controller.class);
-            context.setAttribute("controllers", controllers);
+            // List<Class<?>> controllers = util.getListControllerClass(packageName, Controller.class);
+
+            Map<UrlMethod, RouteMapping> routes = new HashMap<>();
+
+            util.scanControllersInPackage(packageName, routes, Controller.class, UrlMapping.class);
+
+            context.setAttribute("routes", routes);
+            context.setAttribute("prefix", prefix);
+            context.setAttribute("suffix", suffix);
+
+            // context.setAttribute("controllers", controllers);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
