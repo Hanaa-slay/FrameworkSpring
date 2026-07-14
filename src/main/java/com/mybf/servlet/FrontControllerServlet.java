88d18ab4b7fa642fea8c9d8e697a8ecd38755286
Hanaa-slay <hanaatdr@gmail.com>
2026-07-14 10:59:47 +0300

    sprint 5

--- src/main/java/com/mybf/servlet/FrontControllerServlet.java
+++ src/main/java/com/mybf/servlet/FrontControllerServlet.java
@@ -6,6 +6,7 @@ import com.mybf.utils.RouteMapping;
 import com.mybf.utils.UrlMethod;
 import com.mybf.utils.Utilitaire;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -22,31 +23,28 @@ public class FrontControllerServlet extends HttpServlet {
 
     private Utilitaire util;
     private List<Class<?>> controllers;
+    private Map<UrlMethod, RouteMapping> routes;
+    private String suffix;
+    private String prefix;
+
 
 
     public void init(){
         util = new Utilitaire();
-        controllers = (List<Class<?>>) getServletContext().getAttribute("controllers");
-        /* Raha tsy hampiasa listener sinon any ambany */
-//        Utilitaire util = new Utilitaire();
-//        String packageName = getInitParameter("packageController");
-//        try {
-//            listController = util.getListController(packageName, Controller.class);
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-
-        // D ny init-param anaty web.xml ovaina context-param
+        // controllers = (List<Class<?>>) getServletContext().getAttribute("controllers");
+        routes = (Map<UrlMethod, RouteMapping>) getServletContext().getAttribute("routes");
+        prefix = getServletContext().getAttribute("prefix").toString();
+        suffix = getServletContext().getAttribute("suffix").toString();
 
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String projectPath = req.getContextPath();
-        String URL = req.getRequestURI();
+        String URI = req.getRequestURI();
         String method = req.getMethod();
-        URL = URL.substring(projectPath.length());
-        processRequest(resp, URL, method);
+        String URL = URI.substring(projectPath.length());
+        processRequest(req, resp, URL, method);
     }
 
     @Override
@@ -55,60 +53,67 @@ public class FrontControllerServlet extends HttpServlet {
         String URL = req.getRequestURI();
         String method = req.getMethod();
         URL = URL.substring(projectPath.length());
-        processRequest(resp, URL, method);
+        processRequest(req, resp, URL, method);
     }
 
-    private void processRequest(HttpServletResponse res, String url, String method) throws IOException {
 
-        List<String> validUrl = util.getExistingLink(controllers, UrlMapping.class);
-
-        if(!util.isUrlValid(url, validUrl)){
-            StringJoiner sj = new StringJoiner(", ");
-            for(String s: validUrl){
-                sj.add(s);
-            }
-            throw new RuntimeException("L'url que vours avez entré: "+url+" n'est pas valide\n"+"Les url valident sont : "+ sj);
-        }
-
-//        res.setContentType("text/html");
-        PrintWriter out = res.getWriter();
-//        Map<String, List<Method>> methodAssocieUrl = util.getMethodWithUrl(url, UrlMapping.class, controllers);
-//        Map<String, RouteMapping> methodUrl = util.getMethodAssocieUrl(url, UrlMapping.class, controllers);
-
-        Map<UrlMethod, RouteMapping> routes = util.getMethods(url, UrlMapping.class, controllers);
-
-        for(Map.Entry<UrlMethod, RouteMapping> entry: routes.entrySet()){
-            UrlMethod urlMethod = entry.getKey();
-            RouteMapping route = entry.getValue();
-
-            out.println("URL: "+urlMethod.getUrl()+" | method: "+urlMethod.getMethod()+" | Controller: "+route.getController().getName()+" | Method: "+route.getMethod().getName());
-
-            if(method.equals(urlMethod.getMethod())){
-                try{
-                    Object returnValue =  util.invoke(route);
-                    out.println("Valeur retournee: " + returnValue);
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
+    private void processRequest(HttpServletRequest req, HttpServletResponse res, String url, String method) throws IOException, ServletException {
+        UrlMethod um = new UrlMethod(url, method);
+        RouteMapping routeMapping = util.getByUrlMethod(um, routes);
+        if(req.getMethod().equals("GET")){
+            Object objectView = null;
+            if((objectView = util.invoke(routeMapping)) instanceof ModelAndView){
+                ModelAndView modelAndView = (ModelAndView) objectView;
+                Map<String, Object> attributes = modelAndView.getListAttributes();
+                String view = prefix+modelAndView.getUrl()+suffix;  
+                for(Map.Entry<String, Object> entry: attributes.entrySet()){
+                    req.setAttribute(entry.getKey(), entry.getValue());
                 }
+                RequestDispatcher dispatcher = req.getRequestDispatcher(view);
+                dispatcher.forward(req, res);
             }
         }
-
-
-//        for(Map.Entry<String, RouteMapping> entry: methodUrl.entrySet()){
-//            String urlAssocie = entry.getKey();
-//            RouteMapping route = entry.getValue();
-//            out.println("URL: "+url+" | Controller: "+route.getController()+" | Method: "+route.getMethod().getName());
-//        }
-
-//        out.println("URL: "+url);
-
-//        for(Map.Entry<String, List<Method>> entry: methodAssocieUrl.entrySet()){
-//            String clazz = entry.getKey();
-//            List<Method> methods = entry.getValue();
-//            out.println(clazz+":");
-//            for(Method m: methods){
-//                out.println("\t"+m.getName());
-//            }
-//        }
     }
+
+// //        res.setContentType("text/html");
+//         PrintWriter out = res.getWriter();
+// //        Map<String, List<Method>> methodAssocieUrl = util.getMethodWithUrl(url, UrlMapping.class, controllers);
+// //        Map<String, RouteMapping> methodUrl = util.getMethodAssocieUrl(url, UrlMapping.class, controllers);
+
+//         Map<UrlMethod, RouteMapping> routes = util.getMethods(url, UrlMapping.class, controllers);
+
+//         for(Map.Entry<UrlMethod, RouteMapping> entry: routes.entrySet()){
+//             UrlMethod urlMethod = entry.getKey();
+//             RouteMapping route = entry.getValue();
+
+//             out.println("URL: "+urlMethod.getUrl()+" | method: "+urlMethod.getMethod()+" | Controller: "+route.getController().getName()+" | Method: "+route.getMethod().getName());
+
+//             if(method.equals(urlMethod.getMethod())){
+//                 try{
+//                     Object returnValue =  util.invoke(route);
+//                     out.println("Valeur retournee: " + returnValue);
+//                 } catch (Exception e) {
+//                     throw new RuntimeException(e);
+//                 }
+//             }
+//         }
+
+
+// //        for(Map.Entry<String, RouteMapping> entry: methodUrl.entrySet()){
+// //            String urlAssocie = entry.getKey();
+// //            RouteMapping route = entry.getValue();
+// //            out.println("URL: "+url+" | Controller: "+route.getController()+" | Method: "+route.getMethod().getName());
+// //        }
+
+// //        out.println("URL: "+url);
+
+// //        for(Map.Entry<String, List<Method>> entry: methodAssocieUrl.entrySet()){
+// //            String clazz = entry.getKey();
+// //            List<Method> methods = entry.getValue();
+// //            out.println(clazz+":");
+// //            for(Method m: methods){
+// //                out.println("\t"+m.getName());
+// //            }
+// //        }
+//     }
 }
---
+++ src/main/java/com/mybf/servlet/ModelAndView.java
@@ -0,0 +1,30 @@
+package com.mybf.servlet;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ModelAndView {
+    private Map<String, Object> attributes;
+    private String url;
+
+    public ModelAndView(String url){
+        attributes = new HashMap<>();
+        this.url = url;
+    }
+
+    public void setAttribute(String key, Object value){
+        attributes.put(key, value);
+    }
+
+    public Object getAttribute(String key){
+        return attributes.get(key);
+    }
+
+    public Map<String, Object> getListAttributes(){
+        return this.attributes;
+    }
+
+    public String getUrl(){
+        return this.url;
+    }
+}
